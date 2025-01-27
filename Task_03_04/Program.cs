001ef7aeaedf8ee5c4d5e8d9e9f8685982a88f43
reoshik <janaurasheva09@gmail.com>
2025-01-27 17:33:41 +0500

    выполнено задание 4 урока 3

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный текст.");
+
+            int count = 0;  // счетчик введенных строк
+            while (true)
+            {
+                count++;
+                Console.Clear();
+
+                string text = Console.ReadLine();
+
+                // прекращение считывания с консоли 
+                if (text == "exit" || text == "")
+                {
+                    count -= text == "" ? 1 : 0;  // если текст пустой не считать его за введенный 
+
+                    Console.WriteLine($"Вы ввели {count} строк текста!");
+                    break;
+                }
+            }
+        }
+    }
+}
