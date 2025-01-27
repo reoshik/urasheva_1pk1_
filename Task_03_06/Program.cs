bd30b571a2dd518265d91b38a1616ed3a3adef18
reoshik <janaurasheva09@gmail.com>
2025-01-27 17:45:28 +0500

    выполнено задание 6 урока 3

---
+++ Task_03_06/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_03_06
+{
+    internal class Program
+    {
+        //  Написать программу , котторая выводит таблицу значений функции: y=|x|:\n y\tx");
+        static void Main(string[] args)
+        {
+            double y = 0; // значение функции
+
+            Console.WriteLine("Таблица значений функции y=|x|:\n y\tx");
+            for (double i = -4; i <= 4; i += 0.5)
+            {
+                y = Math.Abs(i);
+
+                Console.WriteLine($" {y}\t{i}");
+            }
+        }
+    }
+}
