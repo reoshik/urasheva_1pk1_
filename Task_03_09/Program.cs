5572fd859f7223fdc96341b3bfd746f85b98c820
reoshik <janaurasheva09@gmail.com>
2025-01-27 17:57:28 +0500

    выполнено задание 9 урока 3

---
+++ Task_03_09/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_03_09
+{
+    internal class Program
+    {
+        /*
+         Вклад в банке составляет x рублей. Ежегодно он увеличивается на p процентов, после чего дробная часть 
+        копеек отбрасывается .Каждый год сумма вклада становится больше . Определите, через сколько лет вклад составит 
+        не менее y рублей.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада, годовые проценты и желанную сумму.");
+            double x = Convert.ToDouble(Console.ReadLine());  // Первоначальная сумма
+            double p = Convert.ToDouble(Console.ReadLine());  // Проценты годовых
+            double y = Convert.ToDouble(Console.ReadLine());  // Сумма желанная к получению
+
+            int countYear = 0; //  счётчик лет
+            while (true)
+            {
+                if (x >= y)
+                    break;
+                x += x * p / 100;
+
+                countYear++;
+
+                Console.WriteLine($"Через {countYear}  сколько лет вклад составит не менее {y}руб.");
+            }
+        }
+    }
+}
