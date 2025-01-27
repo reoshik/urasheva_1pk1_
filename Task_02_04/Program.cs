41f13a8bef6dad2dbc7d90a937ac744a78addb30
reoshik <janaurasheva09@gmail.com>
2025-01-27 17:26:28 +0500

    задание 4 урока 2 выполнено

---
+++ Task_02_04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_02_04
+{
+    internal class Program
+    {
+        /* Пользователь вводит свою дату рождения построчно (сначала год, потом месяц и в конце дату)
+         * произведите расчет является ли пользователь совершеннолетним на текущую дату и выведите 
+         * соответствующее сообщение об этом */
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;    // сегоднейшая дата
+
+            Console.WriteLine("Enter your year, mouth and day of birth.");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int mouth = Convert.ToInt32(Console.ReadLine());
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            int age = today.Year - year;    // разница текущего и введённого года
+
+
+            // если введённый день и месяц позже текущего - age--
+            if (today.Month < mouth)
+                age--;
+            else if (today.Month == mouth)
+                age -= today.Day <= day ? 1 : 0;
+
+            if (age >= 18)
+                Console.WriteLine("Отлично!Вы совершеннолетний");
+            else
+                Console.WriteLine(" К сожалению, вы несовершеннолетний");
+
+        }
+    }
+}
