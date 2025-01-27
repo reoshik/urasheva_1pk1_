db6991c099841037a5d2eb5fdba99ea614148726
reoshik <janaurasheva09@gmail.com>
2025-01-27 17:47:41 +0500

    выполнено задание 7 урока 3

---
+++ Task_03_07/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_03_07
+{
+    internal class Program
+    {
+
+        /*
+         Написать программу, которая выводит таблицу скорости (через каждые 0,5с) свободно падающего тела v = g t,
+        где 2 g = 9,8 м/с2 - ускорение свободного падения 
+        */
+        static void Main(string[] args)
+        {
+            double V = 0;  // скорость 
+            double g = 9.8; // ускорение свободного падения
+
+            Console.WriteLine("Введите время падения тела:");
+            double tFinish = Convert.ToDouble(Console.ReadLine());  //  время падения тела 
+
+            for (double i = 0; i <= tFinish; i += 0.5)
+            {
+
+                V = g * i;
+
+
+                Console.WriteLine("Скорость тела: {0}m/c", V);
+            }
+        }
+    }
+}
