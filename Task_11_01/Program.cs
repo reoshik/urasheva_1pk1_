e6e4d8d4ee14c1b16e3dc605c8d0cb61c7677f96
reoshik <janaurasheva09@gmail.com>
2025-02-21 16:30:30 +0500

    Выполнено задание 1 урока 11

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Task_11_01
+{
+    internal class Program
+    {
+        /// <summary>
+        /// меняет числа местами
+        /// </summary>
+        /// <param name="x"> Первое число </param>
+        /// <param name="y"> Второе число </param>
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите число x");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число y");
+            int y = Convert.ToInt32(Console.ReadLine());
+            SwapNums (x, y);
+            Console.WriteLine($"после вызова метода : x ={x} , y = {y}");
+
+            
+        }
+
+        public static void SwapNums(int x, int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
